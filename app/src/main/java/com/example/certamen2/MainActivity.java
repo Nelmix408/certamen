aa98ccbfa27fabed6552a4a3f81531f0a55f1369
Nelmix408 <srwilson199@gmail.com>
2024-11-12 15:45:05 -0300

    segundo push

--- app/src/main/java/com/example/certamen2/MainActivity.java
+++ app/src/main/java/com/example/certamen2/MainActivity.java
@@ -2,16 +2,35 @@ package com.example.certamen2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     Spinner sexo;
+    EditText Peso, Estatura, Resultado;
+    Button CalcularIMC;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Peso = findViewById(R.id.lblPeso);
+        Estatura = findViewById(R.id.lblEstatura);
+        Resultado = findViewById(R.id.lblResultado);
 
+    }
+
+    public void CalcularPeso(View view){
+        Double peso = Double.valueOf(Peso.getText().toString());
+        String estatura = Estatura.getText().toString();
+        Double EstaturaM = Double.parseDouble(estatura)/100.0;
+        Double IMC = peso /(EstaturaM*EstaturaM);
+        if (){
+
+        }
     }
 }

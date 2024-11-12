d09f8d599d26f7b2e7f3a7729ab7d530f126aa25
Nelmix408 <srwilson199@gmail.com>
2024-11-12 14:51:18 -0300

    primer push

---
+++ app/src/main/java/com/example/certamen2/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.certamen2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Spinner;
+
+public class MainActivity extends AppCompatActivity {
+
+    Spinner sexo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
